8b96b883ec7023d7ef4caf75b0bbd9b6088d7ec4
Luis Sanchez <sanchezl@us.ibm.com>
2017-06-20 16:29:03 -0400

    [FAB-3648] add getCreator() API
    Change-Id: I854268bc3f20d8bece9d0ae3f4529e2a62bb67bf
    Signed-off-by: Luis Sanchez <sanchezl@us.ibm.com>

--- shim/src/test/java/org/hyperledger/fabric/shim/impl/ChaincodeStubImplTest.java
+++ shim/src/test/java/org/hyperledger/fabric/shim/impl/ChaincodeStubImplTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.hamcrest.Matchers;
 import org.hyperledger.fabric.protos.common.Common.ChannelHeader;
 import org.hyperledger.fabric.protos.common.Common.Header;
+import org.hyperledger.fabric.protos.common.Common.SignatureHeader;
 import org.hyperledger.fabric.protos.ledger.queryresult.KvQueryResult;
 import org.hyperledger.fabric.protos.ledger.queryresult.KvQueryResult.KV;
 import org.hyperledger.fabric.protos.peer.ChaincodeEventPackage.ChaincodeEvent;
@@ -329,4 +330,30 @@ public class ChaincodeStubImplTest {
 		final ChaincodeStubImpl stub = new ChaincodeStubImpl("txid", handler, new ArrayList<>(), null);
 		assertThat(stub.getTxTimestamp(), is(nullValue()));
 	}
+
+	@Test
+	public void testGetCreator() {
+		final Instant instant = Instant.now();
+		final byte[] creator = "CREATOR".getBytes(UTF_8);
+		final Timestamp timestamp = Timestamp.newBuilder().setSeconds(instant.getEpochSecond()).setNanos(instant.getNano()).build();
+		final SignedProposal signedProposal = SignedProposal.newBuilder()
+				.setProposalBytes(Proposal.newBuilder()
+						.setHeader(Header.newBuilder()
+								.setChannelHeader(ChannelHeader.newBuilder()
+										.setType(ENDORSER_TRANSACTION_VALUE)
+										.setTimestamp(timestamp)
+										.build().toByteString()
+								)
+								.setSignatureHeader(SignatureHeader.newBuilder()
+										.setCreator(ByteString.copyFrom(creator))
+										.build().toByteString()
+								)
+								.build().toByteString()
+						)
+						.build().toByteString()
+				).build();
+		final ChaincodeStubImpl stub = new ChaincodeStubImpl("txid", handler, new ArrayList<>(), signedProposal);
+		assertThat(stub.getCreator(), is(creator));
+	}
+
 }
