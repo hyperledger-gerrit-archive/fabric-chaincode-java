1b863cd0b9752bf8747d5c09be39fa87de93043a
Luis Sanchez <sanchezl@us.ibm.com>
2018-04-05 11:54:15 -0400

    [FAB-9380] remove FSM from java chaincode shim
    - removed old FSM
    - removed old dead code
    - cleaned up / refactored to minimize loc
    - refactored connection logic to enabling running with
      alternate grpc channel builders (useful for test
      and for running with Java9 ALPN in future)
    - tested, but no unit tests written yet.
    
    Change-Id: Ic40aeb7b73a9f421f4012efc3f2273b71a8f4ecd
    Signed-off-by: Luis Sanchez <sanchezl@us.ibm.com>

---
+++ shim/src/main/java/org/hyperledger/fabric/shim/impl/ChaincodeSupportClient.java
@@ -0,0 +1,36 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package org.hyperledger.fabric.shim.impl;
+
+import java.util.concurrent.TimeUnit;
+
+import org.hyperledger.fabric.protos.peer.ChaincodeShim.ChaincodeMessage;
+import org.hyperledger.fabric.protos.peer.ChaincodeSupportGrpc;
+import org.hyperledger.fabric.protos.peer.ChaincodeSupportGrpc.ChaincodeSupportStub;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class ChaincodeSupportClient {
+	
+	private final ManagedChannel channel;
+	private final ChaincodeSupportStub stub;
+	
+	public ChaincodeSupportClient(ManagedChannelBuilder<?> channelBuilder) {
+		this.channel = channelBuilder.build();
+		this.stub = ChaincodeSupportGrpc.newStub(channel);
+	}
+	
+	public void shutdown() throws InterruptedException {
+		this.channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+	}
+
+	public StreamObserver<ChaincodeMessage> register(StreamObserver<ChaincodeMessage> responseObserver) {
+		return stub.register(responseObserver);
+	}
+	
+}
