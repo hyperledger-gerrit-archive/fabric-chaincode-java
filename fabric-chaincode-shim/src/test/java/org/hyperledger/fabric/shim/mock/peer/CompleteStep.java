edbd6b0230ae32158e752c00225dccf76b39b15b
gennady <gennady@il.ibm.com>
2018-07-07 18:05:12 +0300

    [WIP][FAB-10032] Adding unit test
    Added unit test of chaincode flow with mock peer implementation
    
    Change-Id: I6c07cad7d3fc28c9f28687fbc0becb201df7cc9b
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/mock/peer/CompleteStep.java
@@ -0,0 +1,30 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.hyperledger.fabric.shim.mock.peer;
+
+import org.hyperledger.fabric.protos.peer.ChaincodeShim;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompleteStep implements ScenarioStep {
+    @Override
+    public boolean expected(ChaincodeShim.ChaincodeMessage msg) {
+        return msg.getType() == ChaincodeShim.ChaincodeMessage.Type.COMPLETED;
+    }
+
+    @Override
+    public List<ChaincodeShim.ChaincodeMessage> next() {
+        List<ChaincodeShim.ChaincodeMessage> list = new ArrayList<>();
+        return list;
+    }
+
+    @Override
+    public long waitPeriod() {
+        return 0;
+    }
+}
