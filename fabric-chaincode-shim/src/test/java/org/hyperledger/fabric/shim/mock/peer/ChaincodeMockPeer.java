14e6bdeebfc3419fc3083d79bbe2eaae0f974e40
gennady <gennady@il.ibm.com>
2018-07-07 18:05:12 +0300

    [WIP][FAB-10032] Adding unit test
    Added unit test of chaincode flow with mock peer implementation
    
    Change-Id: I6c07cad7d3fc28c9f28687fbc0becb201df7cc9b
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/mock/peer/ChaincodeMockPeer.java
@@ -0,0 +1,147 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.hyperledger.fabric.shim.mock.peer;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import org.hyperledger.fabric.protos.peer.ChaincodeShim;
+import org.hyperledger.fabric.protos.peer.ChaincodeSupportGrpc;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ChaincodeMockPeer {
+    private static final Logger logger = Logger.getLogger(ChaincodeMockPeer.class.getName());
+
+    private final int port;
+    private final Server server;
+    private final List<ScenarioStep> scenario;
+    private final ChaincodeMockPeerService service;
+
+    public ChaincodeMockPeer(List<ScenarioStep> scenario, int port) throws IOException {
+        this.port = port;
+        this.scenario = scenario;
+        this.service = new ChaincodeMockPeerService(this.scenario);
+        ServerBuilder<?> sb = ServerBuilder.forPort(port);
+        this.server = sb.addService(this.service).build();
+    }
+
+    /** Start serving requests. */
+    public void start() throws IOException {
+        server.start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may has been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                ChaincodeMockPeer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    /** Stop serving requests and shutdown resources. */
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    public void send(ChaincodeShim.ChaincodeMessage msg) {
+        this.service.lastMessageSend = msg;
+        this.service.observer.onNext(msg);
+    }
+
+    public int getStartedStep() {
+       return this.service.startStepNumber;
+    }
+
+    public int getEndedStep() {
+        return this.service.endStepNumber;
+    }
+
+    public ChaincodeShim.ChaincodeMessage getLastMessageRcvd() {
+        return this.service.lastMessageRcvd;
+    }
+
+    public ChaincodeShim.ChaincodeMessage getLastMessageSend() {
+        return this.service.lastMessageSend;
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    private static class ChaincodeMockPeerService extends ChaincodeSupportGrpc.ChaincodeSupportImplBase {
+        final List<ScenarioStep> scenario;
+        int startStepNumber;
+        int endStepNumber;
+        ChaincodeShim.ChaincodeMessage lastMessageRcvd;
+        ChaincodeShim.ChaincodeMessage lastMessageSend;
+        StreamObserver<ChaincodeShim.ChaincodeMessage> observer;
+
+
+        public ChaincodeMockPeerService(List<ScenarioStep> scenario) {
+            this.scenario = scenario;
+            this.startStepNumber = 0;
+            this.endStepNumber = 0;
+        }
+
+        @Override
+        public StreamObserver<ChaincodeShim.ChaincodeMessage> register(final StreamObserver<ChaincodeShim.ChaincodeMessage> responseObserver) {
+            observer = responseObserver;
+            return new StreamObserver<ChaincodeShim.ChaincodeMessage>() {
+
+                @Override
+                public void onNext(ChaincodeShim.ChaincodeMessage chaincodeMessage) {
+                    System.err.println("Got message: " + chaincodeMessage);
+                    ChaincodeMockPeerService.this.lastMessageRcvd = chaincodeMessage;
+                    if (ChaincodeMockPeerService.this.scenario.size() > 0) {
+                        ScenarioStep step = ChaincodeMockPeerService.this.scenario.get(0);
+                        ChaincodeMockPeerService.this.scenario.remove(0);
+                        ChaincodeMockPeerService.this.startStepNumber++;
+                        if (step.expected(chaincodeMessage)) {
+                            List<ChaincodeShim.ChaincodeMessage> nextSteps = step.next();
+                            for (ChaincodeShim.ChaincodeMessage m : nextSteps) {
+
+                                if (step.waitPeriod() != 0) {
+                                    try {
+                                        Thread.sleep(step.waitPeriod());
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                ChaincodeMockPeerService.this.lastMessageSend = m;
+                                responseObserver.onNext(m);
+                            }
+                        }
+                        ChaincodeMockPeerService.this.endStepNumber++;
+                    }
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+
+                }
+
+                @Override
+                public void onCompleted() {
+
+                }
+            };
+        }
+    }
+
+}
