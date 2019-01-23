75f789190470ab0454e85661a3e7147901e727a5
gennady <gennady@il.ibm.com>
2019-01-23 14:11:25 +0200

    [WIP][FAB-13794]
    Change-Id: Ia46ac24a55c4e63992bc98229c43e14ea7ac3213
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contact/ContractInterface.java
@@ -0,0 +1,11 @@
+/*
+Copyright IBM Corp., DTCC All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.hyperledger.fabric.contact;
+
+public interface ContractInterface {
+    Context getContext();
+}
