d686cced1bfaccf86c7b2c139075130e4f6b874d
gennady <gennady@il.ibm.com>
2019-01-28 06:52:06 +0200

    [FAB-13794] Define new annotations
    Change-Id: I35b632495139c43c2299f76aad9df748ebabde01
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contact/annotation/Upgrade.java
@@ -0,0 +1,21 @@
+/*
+Copyright IBM Corp., DTCC All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.hyperledger.fabric.contact.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ *  Method level annotation indicating the function to be a called during contract upgrade
+ */
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Upgrade {
+}
