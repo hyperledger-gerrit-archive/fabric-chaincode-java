18e8abb956bba128346fc34a7b9e59a6cbb31ed4
gennady <gennady@il.ibm.com>
2019-01-28 07:08:50 +0200

    [FAB-13795] Contract definition
    Change-Id: I848cd04986a3c3d70d7d1930021c7bfa8075d51e
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contract/ContractInterface.java
@@ -0,0 +1,16 @@
+/*
+Copyright IBM Corp., DTCC All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.hyperledger.fabric.contract;
+
+public interface ContractInterface {
+    default Context getContext() { throw new IllegalStateException("getContext default implementation can't be directly invoked"); }
+    default void unknownTransaction() {
+        throw new IllegalStateException("Undefined contract method called");
+    }
+    default void beforeTransaction() {}
+    default void afterTransaction() {}
+}
