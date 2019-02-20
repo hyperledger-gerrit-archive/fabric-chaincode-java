bd2fd0f2b2527e0dc14a710ae8703515053e7d34
Gennady Laventman <gennady@il.ibm.com>
2019-02-20 16:24:21 +0200

    [WIP][FAB-13195] Netty-tcnative build
    Added netty-tcnative library for platforms that
    don't have it in maven central
    
    Change-Id: I6a4bc0c0c5def762fbf689a388e475268a1235e5
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ buildShim_ppc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./gradlew :fabric-chaincode-protos:build
+./gradlew :fabric-chaincode-shim:build
+./gradlew :fabric-chaincode-docker:copyAllDeps
+cd fabric-chaincode-docker
+docker build -t hyperledger/fabric-javaenv:latest .
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-latest
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-1.3.0
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-1.4.0
