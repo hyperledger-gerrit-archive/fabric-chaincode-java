aaebac6dac57a7c53fb9732728300d90db612995
Gennady Laventman <gennady@il.ibm.com>
2019-02-20 16:24:21 +0200

    [FAB-15092] Netty-tcnative build
    CR build netty-tcnative for both PowerPC and System Z
    
    netty-tcnative library required to run grpc-java with TLS
    and not compiled to platform mentioned above.
    
    in addition, CR include platform specific build scripts, because lack of
    support for java-testcontainers library and gradle-docker-plugin for same
    platforms.
    containers library required for integration tests and docker plugin
    required for fabric-chaincode-docker build.
    
    ignoring protobuf files regenertion in PowerPC and System Z due lack of
    protobuf compilers for those platforms - those platforms will use files
    generated in x86 paltform
    
    reverting from io.grpc:grpc-netty-shaded dependency to io.grpc:grpc-netty
    dependency and updating grpc and protofug dependencies to avoid conflicts
    
    due challanges to build netty-tcnative 2.0.7.Final dependency - need to
    patch during build and patched file checked out from git repo during build,
    updated dependency to 2.0.15.Final
    
    building io.netty:netty-tcnative-boringssl-static dependency in s390x
    require editing some C files that part of external project that checkout
    out from totally different repo during build, so we changed dependency
    (only in s390x) to io.netty:netty-tcnative-openssl-static and should be
    build in any case, as prerequsire to netty-tcnative-boringssl-static
    
    Change-Id: I6a4bc0c0c5def762fbf689a388e475268a1235e5
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ fabric-chaincode-docker/build_netty.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BINTARGETS="ppc64le s390x"
+ZSYSTEM_ARCH="s390x"
+if echo $BINTARGETS | grep -q `uname -m`; then
+	apt --fix-broken install -y
+	apt autoremove -y
+	apt-get install -y git make gcc g++ cmake ninja-build software-properties-common automake autoconf libtool build-essential libapreq2-dev perl libssl-dev
+	export GOPATH="/opt/gopath"
+	export GOROOT="/opt/go"
+	mkdir -p $GOPATH
+	ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
+	GO_VER=1.11.5
+	cd /tmp
+	wget --quiet --no-check-certificate https://storage.googleapis.com/golang/go${GO_VER}.linux-${ARCH}.tar.gz
+	tar -xvf go${GO_VER}.linux-${ARCH}.tar.gz
+	mv go $GOROOT
+	chmod 775 $GOROOT
+	export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+	cd $HOME
+	git clone https://github.com/netty/netty-tcnative.git
+	cd netty-tcnative
+	git checkout netty-tcnative-parent-2.0.15.Final
+	if echo $ZSYSTEM_ARCH | grep -q `uname -m`; then
+        mvn clean install -pl openssl-static -DskipTests
+        cp /root/.m2/repository/io/netty/netty-tcnative-openssl-static/2.0.15.Final/netty-tcnative-openssl-static-2.0.15.Final-linux-s390_64.jar /root/.m2/repository/io/netty/netty-tcnative-openssl-static/2.0.15.Final/netty-tcnative-openssl-static-2.0.15.Final.jar
+	else
+        mvn clean install -pl \!openssl-dynamic -DskipTests
+        cp /root/.m2/repository/io/netty/netty-tcnative-boringssl-static/2.0.15.Final/netty-tcnative-boringssl-static-2.0.15.Final-linux-ppcle_64.jar /root/.m2/repository/io/netty/netty-tcnative-boringssl-static/2.0.15.Final/netty-tcnative-boringssl-static-2.0.15.Final.jar
+	fi
+else
+	exit 0
+fi
+
