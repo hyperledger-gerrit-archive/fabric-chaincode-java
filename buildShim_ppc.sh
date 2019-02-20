8b1ef50bc10508b5fe02433ada9775f347d41241
Gennady Laventman <gennady@il.ibm.com>
2019-02-20 16:24:21 +0200

    [FAB-15092] Netty-tcnative build
    CR build netty-tcnative for both PowerPC and System Z
    
    netty-tcnative library required to run grpc-java with TLS
    and not compiled to platform mentioned above.
    
    in addition, CR include platform specific build scripts, because lack of
    support for java-testcontainers library and gradle-docker-plugin for same
    platforms.
    containers library required for integration tests and docker plugin
    required for fabric-chaincode-docker build.
    
    ignoring protobuf files regenertion in PowerPC and System Z due lack of
    protobuf compilers for those platforms - those platforms will use files
    generated in x86 paltform
    
    reverting from io.grpc:grpc-netty-shaded dependency to io.grpc:grpc-netty
    dependency and updating grpc and protofug dependencies to avoid conflicts
    
    due challanges to build netty-tcnative 2.0.7.Final dependency - need to
    patch during build and patched file checked out from git repo during build,
    updated dependency to 2.0.15.Final
    
    building io.netty:netty-tcnative-boringssl-static dependency in s390x
    require editing some C files that part of external project that checkout
    out from totally different repo during build, so we changed dependency
    (only in s390x) to io.netty:netty-tcnative-openssl-static and should be
    build in any case, as prerequsire to netty-tcnative-boringssl-static
    
    Change-Id: I6a4bc0c0c5def762fbf689a388e475268a1235e5
    Signed-off-by: gennady <gennady@il.ibm.com>

---
+++ buildShim_ppc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#./gradlew :fabric-chaincode-protos:build
+#./gradlew :fabric-chaincode-shim:build
+./gradlew :fabric-chaincode-docker:copyAllSrc
+cd fabric-chaincode-docker
+docker build -t hyperledger/fabric-javaenv:latest .
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-latest
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-2.0.0
+docker tag hyperledger/fabric-javaenv:latest hyperledger/fabric-javaenv:ppc64le-1.4.0
