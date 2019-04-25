3993253ff22395728d0e277cfad26d2f715f6480
Matthew B. White <whitemat@uk.ibm.com>
2019-04-25 14:10:34 +0100

    [FAB-13802] Metadata Support
    - Added a classes to represent logical aspects of the contracts;
      these are stored in 2 registries
    - Metadata Builder uses these classes to create the JSON form of
      the metadata
    - As does the router to get access to the code to execute
    - Added a Contract specific runtime exception to help with
      explaining to users what went wrong
    - Found that the Commons logging never picked up on the debug
      statements; so added a simple wrapper
      to route via standard java.util.logging. also allowed some
      extra processing for exceptions
    - Adjusted a number of the log points to use lambdas to improve
      performance
    - White space removal
    
    Change-Id: I9752b2e7c2ba62f521abbdbfcc86b3c4f2baa693
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

---
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contract/routing/TxFunction.java
@@ -0,0 +1,34 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package org.hyperledger.fabric.contract.routing;
+
+import java.lang.reflect.Method;
+
+import org.hyperledger.fabric.contract.ContractInterface;
+
+public interface TxFunction {
+	
+	public interface Routing {
+	    ContractInterface getContractObject();
+
+	    Method getMethod();
+
+	    Class getContractClass();
+
+//	    TransactionType getType();
+	}
+
+	String getName();
+
+    Routing getRouting();
+
+	Class<?> getReturnType();
+
+	java.lang.reflect.Parameter[] getParameters();
+
+	TransactionType getType();
+
+}
