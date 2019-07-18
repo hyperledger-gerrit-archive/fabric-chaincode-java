a5b29a5600a74f4b24d73a3bc0e652c48c11e06e
heatherlp <heatherpollard0@gmail.com>
2019-07-18 11:10:49 +0100

    [FAB-15895] Added client identity to context
    - Created a test utility file for storing certificate strings
    - Moved tests from ContractSimplePath to ContractRouterTest
    - Tidied up some imports
    - Fixed some typos and warnings
    
    Signed-off-by: heatherlp <heatherpollard0@gmail.com>
    Change-Id: Iee2512ca4078a6487f6711eb5eae0be5b82acd30

--- fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contract/ContextFactory.java
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/contract/ContextFactory.java
@@ -6,7 +6,11 @@ SPDX-License-Identifier: Apache-2.0
 
 package org.hyperledger.fabric.contract;
 
+import java.io.IOException;
+import java.security.cert.CertificateException;
+
 import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.json.JSONException;
 
 /**
  * Factory to create {@link Context} from {@link ChaincodeStub} by wrapping stub
@@ -24,6 +28,12 @@ public class ContextFactory {
 
     public Context createContext(final ChaincodeStub stub) {
         Context newContext = new Context(stub);
+        try {
+            newContext.setClientIdentity(new ClientIdentity(stub));
+        } catch (CertificateException | JSONException | IOException e) {
+            throw new ContractRuntimeException("Could not create new client identity", e);
+        }
+
         return newContext;
     }
 
