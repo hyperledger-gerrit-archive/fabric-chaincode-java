625e05b6b5a676cbd2c53a01f92041c8ceba6f4c
Matthew B. White <whitemat@uk.ibm.com>
2019-08-21 14:29:30 +0100

    [FAB-16315] Improved Load Ability
    A fairly sizeable CR for improving the load handling
    ability of the Java chaincode. This required a rework
    of the threading model within the core shim layer.
    
    Plus adding configuration and metrics to support this.
    o
    
    Change-Id: I31b05585a0aa650f7e2a7e2b0389799e90adc2c3
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

--- fabric-chaincode-integration-test/src/test/java/org/hyperleder/fabric/shim/integration/SACCIntegrationTest.java
+++ fabric-chaincode-integration-test/src/test/java/org/hyperleder/fabric/shim/integration/SACCIntegrationTest.java
@@ -27,7 +27,7 @@ public class SACCIntegrationTest {
     public static DockerComposeContainer env = new DockerComposeContainer(
             new File("src/test/resources/first-network/docker-compose-cli.yaml")
     )
-            .withLocalCompose(false)
+            .withLocalCompose(true)
             .withPull(true);
 
     @BeforeClass
