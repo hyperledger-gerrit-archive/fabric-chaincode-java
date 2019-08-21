846c1583ed4b53234975cc3cafc7b90de3143093
Matthew B. White <whitemat@uk.ibm.com>
2019-08-21 14:29:30 +0100

    [FAB-16315] Improved Load Ability
    A fairly sizeable CR for improving the load handling
    ability of the Java chaincode. This required a rework
    of the threading model within the core shim layer.
    
    Plus adding configuration and metrics to support this.
    o
    
    Change-Id: I31b05585a0aa650f7e2a7e2b0389799e90adc2c3
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

---
+++ fabric-chaincode-shim/src/main/java/org/hyperledger/fabric/Logging.java
@@ -0,0 +1,42 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package org.hyperledger.fabric;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * Logger class to use throughout the Contract Implementation
+ *
+ */
+public class Logging {
+
+    public static final String PERFLOGGER = "org.hyperledger.Performance";
+    
+	public static Logger getLogger(Class<?> class1) {
+		// important to add the logger to the log manager
+		java.util.logging.Logger l = java.util.logging.Logger.getLogger(class1.getName());
+		return l;
+	}
+
+	public static String formatError(Throwable throwable) {
+		if (throwable == null)
+			return null;
+		final StringWriter buffer = new StringWriter();
+		buffer.append(throwable.getMessage());
+		throwable.printStackTrace(new PrintWriter(buffer));
+
+		Throwable cause = throwable.getCause();
+		if (cause != null) {
+			buffer.append(".. caused by ..");
+			buffer.append(Logging.formatError(cause));
+		}
+
+		return buffer.toString();
+
+	}
+}
